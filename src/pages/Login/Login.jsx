e02a9ab4c4a93fa9f30a543f9b26c59dc92be5f8
Anas Firly <anasfirly20@gmail.com>
2023-05-14 19:06:50 +0300

    adjusted

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,7 +6,10 @@ import Input from "../../Components/Input";
 // Utils
 import { clearObject } from "../../../utils";
 
-const Login = () => {
+// Miscellaneous
+import { Helmet } from "react-helmet-async";
+
+const Login = ({ title }) => {
   const [data, setData] = useState({
     idInstance: "",
     ApiTokenInstance: "",
@@ -31,37 +34,42 @@ const Login = () => {
   };
 
   return (
-    <section className="min-h-screen bg-customBg flex justify-center items-start py-longer text-customWhite">
-      <form>
-        <h1 className="text-center font-semibold text-3xl mb-3">LOGIN</h1>
-        <h3>
-          Enter your Id Instance and Api Token Instance from your green-api
-          account
-        </h3>
-        <div className="grid gap-5 mt-10">
-          <Input
-            label="Id Instance"
-            name="idInstance"
-            value={data?.idInstance}
-            onChange={handleChange}
-          />
-          <Input
-            label="Api Token Instance"
-            name="ApiTokenInstance"
-            value={data?.ApiTokenInstance}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="text-center">
-          <button
-            className="text-customWhite mt-10 w-[70%] py-2 font-semibold text-lg bg-customTealGreen rounded-lg"
-            onClick={handleClick}
-          >
-            SUBMIT
-          </button>
-        </div>
-      </form>
-    </section>
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <section className="min-h-screen bg-customBg flex justify-center items-start py-longer text-customWhite">
+        <form>
+          <h1 className="text-center font-semibold text-3xl mb-3">LOGIN</h1>
+          <h3>
+            Enter your Id Instance and Api Token Instance from your green-api
+            account
+          </h3>
+          <div className="grid gap-5 mt-10">
+            <Input
+              label="Id Instance"
+              name="idInstance"
+              value={data?.idInstance}
+              onChange={handleChange}
+            />
+            <Input
+              label="Api Token Instance"
+              name="ApiTokenInstance"
+              value={data?.ApiTokenInstance}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="text-center">
+            <button
+              className="text-customWhite mt-10 w-[70%] py-2 font-semibold text-lg bg-customTealGreen rounded-lg"
+              onClick={handleClick}
+            >
+              SUBMIT
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
